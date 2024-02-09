2405879a53b9a7ca7718164cf6fb5f70e9b702a0
Sabohatfrontend <ssobirova333@gmail.com>
2024-02-09 14:53:42 +0500

    feat: add up and cd module

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import * as readline from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process';
+import { handleCommand, getCurrentDir } from './modules/index.js';
+
+const args = process.argv;
+
+const userName = args
+    .slice(2)
+    .find((value) => value.indexOf('--username') === 0)
+    .replace('--username=', '');
+
+console.log(`Welcome to the File Manager, ${userName || 'stranger'}!`);
+getCurrentDir();
+
+const rl = readline.createInterface({ input, output });
+
+rl.on('line', (line) => {
+    handleCommand(line);
+})
+    .on('history', (history) => { })
+    .on('SIGINT', () => {
+        rl.close();
+    })
+    .on('close', () => {
+        console.log(`Thank you for using File Manager, ${userName || 'stranger'}, goodbye!`);
+    })
---
+++ src/modules/index.js
@@ -0,0 +1,26 @@
+import getCurrentDir from './getCurrentDir.js';
+import up from './up.js';
+import cd from './cd.js';
+
+const handleCommand = (fullCommand) => {
+    const [command, ...commandArgs] = fullCommand.toLowerCase().trim().split(' ');
+    switch (command) {
+        case 'up': up(); break;
+        case 'cd': cd(commandArgs[0]); break;
+        case 'ls': console.log('ls'); break;
+        case 'cat': console.log('cat'); break;
+        case 'add': console.log('add'); break;
+        case 'rn': console.log('rn'); break;
+        case 'cp': console.log('cp'); break;
+        case 'mv': console.log('mv'); break;
+        case 'rm': console.log('rm'); break;
+        case 'os': console.log('os'); break;
+        case 'hash': console.log('hash'); break;
+        case 'compress': console.log('compress'); break;
+        case 'decompress': console.log('decompress'); break;
+        default: break;
+    }
+    getCurrentDir();
+}
+
+export { handleCommand, getCurrentDir };
